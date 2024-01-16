8cd056c8637764957e575d890e853426b9b74e0a
22ericchen <22ericchen@gmail.com>
2024-01-16 15:25:14 -0500

    created new .NET project and class library. Added FlatVector class and implimented the bector addition operator.

---
+++ FlatPhysics/FlatPhysics/FlatVector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace FlatPhysics;
+
+public class FlatVector
+{
+    public readonly float X;
+    public readonly float Y;
+    
+    public FlatVector (float x, float y) 
+    {
+        this.X = x;
+        this.Y = y;
+    }   
+
+    public static FlatVector operator + (FlatVector a, FlatVector b) 
+    {
+        return new FlatVector(a.X + b.X, a.Y + b.Y);    
+    }
+}
